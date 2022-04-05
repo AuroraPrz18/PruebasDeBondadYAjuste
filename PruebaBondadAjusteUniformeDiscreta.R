97d3cf6b10da2162215c2e1ca863ca3ba8feab63
AuroraPrz18 <auro.prz1809@gmail.com>
2022-04-04 22:45:51 -0500

    Prueba uniforme discreta completa

---
+++ PruebaBondadAjusteUniformeDiscreta.R
@@ -0,0 +1,52 @@
+library(readxl)
+x <- read.csv("C:/Users/Aurora/Desktop/New folder (2)/Simulacion/final/PruebasBondadAjuste/uniforme.csv",
+              header = FALSE,
+              sep = ",")   
+
+
+G <- x[,1] ## Una distribucion generada (los numeros que vienen de nuestro programa), manda primera columna
+tamano <- length(G)
+## Generación de números pseudoaleatorios binomiales
+if(!require('extraDistr')) {
+  install.packages('extraDistr')
+  library('extraDistr')
+}
+H <-rdunif(tamano, 
+           10, #min
+           20) #max
+
+d <- (1.36)/sqrt(length(G))
+ks.test(G, H)    ## compara que tanto se parece
+
+#install.packages("ggplot2")
+library(ggplot2)
+
+#Graficacion en lugar de hist(G)
+qplot(G,
+      geom="histogram", 
+      main = "Histograma de G", 
+      xlab = "X",  
+      ylab = "Frecuencia", 
+      fill=I("blue"), 
+      col=I("black"), 
+      alpha=I(.2))
+
+
+#Graficacion en lugar de hist(H)
+qplot(H,
+      geom="histogram", 
+      main = "Histograma de H", 
+      xlab = "X",  
+      ylab = "Frecuencia", 
+      fill=I("pink"), 
+      col=I("black"), 
+      alpha=I(.2))
+
+
+qqplot(G, H, main="Comparación de datos generados con datos hipotéticos",
+       xlab="Datos hipotéticos (G)",
+       ylab = "Datos generados (H)",
+       col=I("blue"))
+
+
+
